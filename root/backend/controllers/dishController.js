42994ad54c163f99181a0e1d176cfd8f4c5a7a7b
LuongHongTienDat <71415613+LuongHongTienDat@users.noreply.github.com>
2022-03-16 09:27:30 +0700

    update

--- root/backend/controllers/dishController.js
+++ root/backend/controllers/dishController.js
@@ -1,6 +1,8 @@
 const Dish = require('../models/dishModel');
 
 class DishController {
+    
+//  [ GET - ROUTE: /:id ]
     async getDishByID(req,res){
         var dish = await Dish.findById(req.params.id);
         if (dish){
@@ -10,6 +12,40 @@ class DishController {
             res.status(404);
             throw new Error('Dish does not exist!');
         }
+    }
+
+//  [ GET - ROUTE: /cate/:cateName ]
+    async getDishesByCate(req,res){
+        var dishes = await Dish.find({category: req.params.nameCate});
+        if (dishes){
+            res.json(dishes);
+        }
+        else {
+            res.status(404);
+            throw new Error('Category does not exist!');
+        }
+    }
+
+//  [ POST - ROUTE: / (updated soon) ]
+    async createDish(req,res){
+        const { name, category, dishDetails, image, price, countInStock } = req.body;
+        const newProduct = await Product.create({
+            name,
+            image,
+            category,
+            dishDetails,
+            price,
+            countInStock
+        });
+        if (newDish) {
+            await newDish.save();
+            res.json(newDish);
+        }
+        else {
+            res.status(404);
+            throw new Error("Invalid data");
+        }              
+
     }
 }
 
