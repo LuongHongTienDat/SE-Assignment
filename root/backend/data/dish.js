29c844f4853d0f5fd74abea6ca064b68d4967668
Thiên Phú <79859348+devthienphu@users.noreply.github.com>
2022-03-19 21:47:06 +0700

    Update dish.js

--- root/backend/data/dish.js
+++ root/backend/data/dish.js
@@ -1,9 +1,10 @@
 const dish =[
     {
+        id:1,
         name:'Combo Sushi',
         image:'https://pngimg.com/uploads/sushi/sushi_PNG9202.png',
         category:'sushi',
-        dishDetails:'4 Combo Thuyền Sushi Dành Cho 2 Người.',
+        dishDetails:'4 Combo Thuyền Sushi Dành Cho 2 Người.Thêm gần nhau hơn',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -11,9 +12,11 @@ const dish =[
         reviews:'',
         dishDescription:'Bao gồm 4 loại sushi: Sushi cá hồi,Sushi cuộn cali, Sushi cuộn da cá hồi., Sushi cuộn cá hồi.',
         countInStock:'',
-        isBestSeller:'true'
+        isCheck:false,
+        isBestSeller:true
     },
     {
+        id:2,
         name:'Oshizushi',
         image:'https://vntopfood.com/wp-content/uploads/2022/02/Cach-lam-sushi-ca-hoi-1-min-1024x553.png',
         category:'sushi',
@@ -25,13 +28,15 @@ const dish =[
         reviews:'',
         dishDescription:'Được làm bằng cách cho các lớp Neta và Shari, hần Neta bao gồm các loại cá và hải sản như cá thu hoặc cá hồi; với hình dạng đáo này mà Oshizushi thường được lựa chọn thưởng thức như cơm hộp Bento hoặc quà tặng.',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:3,
         name:'Tempura',
         image:'https://www.pikpng.com/pngl/b/398-3984487_click-to-enlarge-tempura-clipart.png',
         category:'sushi',
-        dishDetails:'Tempura món chiên gồm có hải sản và rau củ, được tẩm với bột tempura rồi đem chiên ngập dầu trong chảo',
+        dishDetails:'Tempura món chiên gồm có hải sản và rau củ, được tẩm với bột tempura',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -39,13 +44,15 @@ const dish =[
         reviews:'',
         dishDescription:'Đây là một món ăn đặc trưng góp phần đưa ẩm thực Nhật Bản lan tỏa mạnh mẽ hơn trên thế giới. Cùng với Sushi, Tempura “đánh thức” mọi giác quan của người dùng bằng sự tinh tế, hài hòa và phong phú trong hương vị.',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:4,
         name:'Chirashizushi',
         image:'https://www.kikkoman.com/en/foodforum/img/recipe-t-31-1-01.png',
         category:'sushi',
-        dishDetails:'ới phần Neta là các hải sản phổ biến như cá hồi, mực, tôm luộc cùng cùng trứng chiên cắt nhỏ và dưa chuột',
+        dishDetails:'Với phần Neta là các hải sản phổ biến như cá hồi, mực, tôm luộc',
         price:'199.000',
         countRating:'',
         rating:'',
@@ -53,13 +60,15 @@ const dish =[
         reviews:'',
         dishDescription:'Chirashizushi là một bát cơm với nhiều loại nguyên liệu khác nhau, với phần Neta là các hải sản phổ biến như cá hồi, mực, tôm luộc cùng cùng trứng chiên cắt nhỏ và dưa chuột. Món ăn tương tự như Kaisendon (Cơm hải sản); sự khiến biệt giữa hai món ăn là Chirashizushi sử dụng Shari – cơm Sushi trộn giấm, còn Kaisendon sử dụng cơm trắng hấp chín.',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:5,
         name:'Gukan Maki',
         image:'https://www.ayasushi.fr/wp-content/uploads/2016/09/oeufsdesaumon-mini-gunkan-300x300.png',
         category:'sushi',
-        dishDetails:'Neta phổ biến cho món Gunkan Maki thường là nhím biển, mực, trứng cá hồi, Negitoro (hỗn hợp của cá ngừ băm và hành lá), salad khoai tây và Kanimiso (gạch cua trộn Miso).',
+        dishDetails:'Neta phổ biến cho món Gunkan Maki thường là nhím biển, mực, trứng cá hồi',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -67,13 +76,15 @@ const dish =[
         reviews:'',
         dishDescription:'Món ăn được làm bằng cách quấn một dải Nori rộng xung quanh một nắm cơm trong khi để lại đủ không gian ở trên cùng để lắp đầu bằng phần nhân Neta khác nhau. Gukan được hiểu là “chiến hạm” hoặc “tàu chiến”, bắt nguồn từ hình dạng giống như một con tàu nhỏ của nó.',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:6,
         name:'Soup bí đỏ',
         image:'https://bizweb.dktcdn.net/thumb/grande/100/431/174/products/005.png?v=1627544849563',
         category:'soups',
-        dishDetails:'Súp mềm mịn, thơm mùi bí đỏ và béo ngậy hương vị sữa sẽ là món ăn đổi vị trong những ngày này.',
+        dishDetails:'Súp mềm mịn, thơm mùi bí đỏ và béo ngậy hương vị sữa ',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -81,13 +92,15 @@ const dish =[
         reviews:'',
         dishDescription:'Súp bí đỏ thơm ngon, mang chút béo ngậy của sữa và whipping cream sẽ là món ăn đổi vị cho những ngày này. Súp bỉ đỏ ăn kèm với bánh mì nướng là sự kết hợp hoàn hảo và ngon miệng. ',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:7,
         name:'Soup cua',
         image:'https://salt.tikicdn.com/cache/w1200/ts/product/e4/3b/54/8c56ac7bfc2f45bba05efcde966d9250.png',
         category:'soups',
-        dishDetails:'Những ngày cuối năm se lạnh, thật tuyệt vời khi cả gia đình quây quần bên nhau, thưởng thức chén súp nóng hổi, thơm ngon',
+        dishDetails:'Cả gia đình quây quần bên nhau, thưởng thức chén súp nóng hổi, thơm ngon',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -95,13 +108,15 @@ const dish =[
         reviews:'',
         dishDescription:'Súp cua với nhiều màu sắc bắt mắt của cua và các loại đậu, rau củ đi kèm, đặc biệt súp cua lại giàu chất dinh dưỡng và nhiều canxi rất tốt cho sức khỏe. Món này ăn vào những ngày trời lạnh là ngon nhất, có thể dùng làm món khai vị trong các bữa tiệc cũng rất hợp.',
         countInStock:'',
-        isBestSeller:'true'
+        isCheck:false,
+        isBestSeller:true
     },
     {
+        id:8,
         name:'Soup nấm',
         image:'https://product.hstatic.net/1000190406/product/mixed_mushroom_menu_411127a13a254129a684eca63563180f_large.png',
         category:'soups',
-        dishDetails:'Súp kem nấm mang hương vị đậm đà, xen lẫn vị tươi ngon tự nhiên từ nấm hương, bị beo béo từ kem tươi',
+        dishDetails:'Súp kem nấm mang hương vị đậm đà, xen lẫn vị tươi ngon tự nhiên từ nấm hương',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -109,13 +124,15 @@ const dish =[
         reviews:'',
         dishDescription:'súp kem nấm mang hương vị đậm đà, xen lẫn vị tươi ngon tự nhiên từ nấm hương, bị beo béo từ kem tươi. Khi ăn sẽ cảm nhận được mùi hương thơm dịu từ xạ hương và ngò, tạo nên hương vị cuốn hút khó cưỡng. Ăn kèm với lát bánh mì thì chuẩn vị nhé!',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:9,
         name:'Soup lá cẩm nấm tuyết gà xé',
         image:'https://product.hstatic.net/1000206480/product/sup_la_cam_d831f3ab2d9d49d78eb350b252c1aa23_1024x1024.png',
         category:'soups',
-        dishDetails:'Với vị ngọt thanh mát từ nước thịt gà luộc, nấm tuyết thơm nhiều dinh dưỡng có độ dai sần sật khi ăn khá vui miệng',
+        dishDetails:'Với vị ngọt thanh mát từ nước thịt gà luộc, nấm tuyết thơm nhiều dinh dưỡng',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -123,13 +140,15 @@ const dish =[
         reviews:'',
         dishDescription:'Món súp lá cẩm nấm tuyết gà xé là sự kết hợp độc đáo của nhiều nguyên liệu. Với vị ngọt thanh mát từ nước thịt gà luộc, nấm tuyết thơm nhiều dinh dưỡng có độ dai sần sật khi ăn khá vui miệng. Các đầu bếp tại Feast Box đã sử dụng lá cẩm có màu tím "mộng mơ" tạo màu sắc nổi bật cho món súp thêm phần hấp dẫn kích thích thị giác.',
         countInStock:'',
-        isBestSeller:'true'
+        isCheck:false,
+        isBestSeller:true
     },
     {
+        id:10,
         name:'Coca-Cola',
         image:'https://popeyes.vn/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/o/coca.png',
         category:'drinks',
-        dishDetails:'CacaCola vị nguyên bản',
+        dishDetails:'CacaCola vị nguyên bản, cho bạn cảm giác sảng khoái',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -137,9 +156,11 @@ const dish =[
         reviews:'',
         dishDescription:'CacaCola vị nguyên bản',
         countInStock:'',
-        isBestSeller:''
+        isCheck:false,
+        isBestSeller:true
     },
     {
+        id:11,
         name:'Trà đào',
         image:'https://shopnguyenlieu.com/wp-content/uploads/2019/09/tradaothachdao-550x467.png',
         category:'drinks',
@@ -151,13 +172,15 @@ const dish =[
         reviews:'',
         dishDescription:'Khi nhiệt độ ở Sài Gòn tiệm cận con số 40 thì trà đào ướp lạnh bỗng dưng trở thành món giải khát cứu cánh. Trà đào thì cũng có trà đào “this”, trà đào “that”',
         countInStock:'',
-        isBestSeller:'true'
+        isCheck:false,
+        isBestSeller:true
     },
     {
+        id:12,
         name:'Vodka',
         image:'https://product.hstatic.net/1000206480/product/sup_la_cam_d831f3ab2d9d49d78eb350b252c1aa23_1024x1024.png',
         category:'drinks',
-        dishDetails:'Rượu hảo hạng đến từ Nga',
+        dishDetails:'Rượu hảo hạng đến từ Nga, buổi tiệc thêm vui',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -165,13 +188,15 @@ const dish =[
         reviews:'',
         dishDescription:'Thức uống giúp bạn tăng cường thể lực như khi Nga đánh Ukraine',
         countInStock:'',
-        isBestSeller:''
+        isCheck:false,
+        isBestSeller:true
     },
     {
+        id:13,
         name:'Pepsi',
         image:'http://scontent.webpluscnd.net/photos-df/a-0/7714-1688041-1/pepsi.png?atk=e37811ba2046832cb285a8377910107c',
         category:'drinks',
-        dishDetails:'Pepsi vị nguyên bản',
+        dishDetails:'Pepsi vị nguyên bản, mát lạnh sảng khoái như CoCa',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -179,13 +204,15 @@ const dish =[
         reviews:'',
         dishDescription:'Nếu bạn không tích Coca nguyên bản thì bạn đã có Pesi vị nguyên bản',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:14,
         name:'Kem dừa',
         image:'https://kemmiennhietdoi.com/timthumb.php?src=upload/product/kem-dua1603442684_300x300.png&w=300&h=300&zc=2&q=85',
         category:'dessert',
-        dishDetails:'Thời tiết nắng nóng nếu có một trái dừa chứa đầy ắp kem mát lạnh bên trong để thưởng thức thì thật tuyệt đúng không nào',
+        dishDetails:' một trái dừa chứa đầy ắp kem mát lạnh bên trong để thưởng thức thì thật tuyệt',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -193,13 +220,15 @@ const dish =[
         reviews:'',
         dishDescription:'Những viên kem mềm mịn, béo ngậy hòa cùng với lớp sốt socola ngọt ngào, thêm chút bùi bùi của đậu phộng rang và dừa sấy khô đảm bảo sẽ khiến bất kì ai đã nếm qua đều không thể cưỡng lại được.',
         countInStock:'',
-        isBestSeller:'true'
+        isCheck:false,
+        isBestSeller:true
     },
     {
+        id:15,
         name:'Pudding',
         image:'https://www.nicepng.com/png/full/95-951391_caramel-pudding-cream-caramel-pudding-png.png',
         category:'dessert',
-        dishDetails:'Bánh pudding với vị thơm ngon mềm mịn thường được dùng để ăn tráng miệng hoặc topping trà sữa',
+        dishDetails:'Thơm ngon mềm mịn thường được dùng để ăn tráng miệng hoặc topping trà sữa',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -207,9 +236,11 @@ const dish =[
         reviews:'',
         dishDescription:'Bánh pudding là loại bánh lạnh được làm đông bởi gelatin kết hợp với một số hương vị khác nhau như vị trà xanh, vị dâu, vị cam,Bánh pudding với vị thơm ngon mềm mịn thường được dùng để ăn tráng miệng hoặc topping trà sữa',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:16,
         name:'Bánh flan',
         image:'https://product.hstatic.net/200000388411/product/banhflan-1024_6393ed364fcb4b7587298dabb8b2d78a.png',
         category:'dessert',
@@ -221,13 +252,15 @@ const dish =[
         reviews:'',
         dishDescription:'ăn nguội bằng cách để lạnh trong tủ lạnh trước khi ăn. Bánh được trút lên đĩa để phần có đường thắng màu cánh gián trên miếng bánh quay lên trên. ',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:17,
         name:'Gà rán',
         image:'https://fujimart.vn/image/cache/catalog/delica/dui%20ga%20chien-502x502.png',
         category:'fastfood',
-        dishDetails:'Món gà rán giòn tan thơm ngon mọng nước được hầu hết giới trẻ đặc biệt yêu thích',
+        dishDetails:'Gà rán giòn tan thơm ngon mọng nước được hầu hết giới trẻ đặc biệt yêu thích',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -235,13 +268,15 @@ const dish =[
         reviews:'',
         dishDescription:'Gà rán vàng ruộm rất bắt mắt, lớp vỏ thì giòn rụm hòa quyện thịt gà mềm ngọt bên trong. Gà rán không cay nên phù hợp cho tất cả các thành viên trong gia đình, đặc biệt là các em nhỏ.',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:18,
         name:'Burger',
         image:'https://freepngimg.com/thumb/burger/22388-5-burger-food.png',
         category:'fastfood',
-        dishDetails:'Món gà rán giòn tan thơm ngon mọng nước được hầu hết giới trẻ đặc biệt yêu thích',
+        dishDetails:'Burger bò siêu ngon hấp dẫn, đậm vị burger',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -249,13 +284,15 @@ const dish =[
         reviews:'',
         dishDescription:'Gà rán vàng ruộm rất bắt mắt, lớp vỏ thì giòn rụm hòa quyện thịt gà mềm ngọt bên trong. Gà rán không cay nên phù hợp cho tất cả các thành viên trong gia đình, đặc biệt là các em nhỏ.',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
     {
+        id:19,
         name:'Pizza',
         image:'https://www.seekpng.com/png/full/1-16481_pizza-png-photo.png',
         category:'fastfood',
-        dishDetails:'Mặt trên bánh thường có nhiều loại nguyên liệu khác nhau tạo thành nhiều màu sắc hấp dẫn.',
+        dishDetails:'Nhiều loại nguyên liệu khác nhau tạo thành nhiều màu sắc hấp dẫn.',
         price:'199.000',
         countRating:'5',
         rating:'',
@@ -263,9 +300,11 @@ const dish =[
         reviews:'',
         dishDescription:'Pizza hải sản phô mai là một món nướng với sự kết hợp hài hoà giữa vị tươi ngon của hải sản và vị thơm béo từ phô mai tạo nên vị ngon khó cưỡng,',
         countInStock:'',
-        isBestSeller:'true'
+        isCheck:false,
+        isBestSeller:true
     },
     {
+        id:20,
         name:'Khoai tây chiên',
         image:'https://mcdonalds.vn/uploads/2018/food/ga-ran/large_world_famous_fries.png',
         category:'fastfood',
@@ -277,10 +316,7 @@ const dish =[
         reviews:'',
         dishDescription:'khoai tây chiên ngoài giòn rụm trong mềm thơm, ngon hết xảy luôn',
         countInStock:'',
+        isCheck:false,
         isBestSeller:''
     },
-
-
-
-
 ]
